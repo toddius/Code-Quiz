063fb47aa3a37e00fa32b6b2d71e19aaeb6871fc
Todd Walls <t_e_walls@yahoo.com>
2021-01-23 09:58:10 -0700

    Added working countdown timer that is visible in the nav bar. I still need to figure out how to subtract 10 seconds when a wrong answer is chosen, I need to figure out how to end game when time runs out. I needto figure out how to determine when a correct or wrong answer is chosen and move to the next question. I need to add another div for entering initials and store to local storage when game over. I need to start CSS.

--- Code Quiz/Assets/JS/script.js
+++ Code Quiz/Assets/JS/script.js
@@ -1,49 +1,59 @@
+let timeLeft = 0;
+
 document.getElementById("startButton").addEventListener("click", startGame);
 
 function startGame() {
   document.getElementById("gameIntro").innerHTML = document.getElementById("question1").innerHTML;
+
+    timeleft = 75;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countDownTimer").innerHTML = timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);
 }
 
-document.getElementById("q1Correct").addEventListener("click", question1Correct);
+// document.getElementById("q1Correct").addEventListener("click", question1Correct);
 
-document.getElementsByClassName("q1Wrong").addEventListener("click", question1Wrong);
+// document.getElementsByClassName("q1Wrong").addEventListener("click", question1Wrong);
 
-function question1Correct() {
-  document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
-}
-function question1Wrong() {
-  document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
-}
+// function question1Correct() {
+//   document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
+// }
+// function question1Wrong() {
+//   document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
+// }
 
-document.getElementById("q2Correct").addEventListener("click", question2Correct);
+// document.getElementById("q2Correct").addEventListener("click", question2Correct);
 
-document.getElementsByClassName("q2Wrong").addEventListener("click", question2Wrong);
+// document.getElementsByClassName("q2Wrong").addEventListener("click", question2Wrong);
 
-function question2Correct() {
-  document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
-}
-function question2Wrong() {
-  document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
-}
+// function question2Correct() {
+//   document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
+// }
+// function question2Wrong() {
+//   document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
+// }
 
-document.getElementById("q3Correct").addEventListener("click", question3Correct);
+// document.getElementById("q3Correct").addEventListener("click", question3Correct);
 
-document.getElementsByClassName("q3Wrong").addEventListener("click", question3Wrong);
+// document.getElementsByClassName("q3Wrong").addEventListener("click", question3Wrong);
 
-function question3Correct() {
-  document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
-}
-function question3Wrong() {
-  document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
-}
+// function question3Correct() {
+//   document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
+// }
+// function question3Wrong() {
+//   document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
+// }
 
-document.getElementById("q4Correct").addEventListener("click", question14orrect);
+// document.getElementById("q4Correct").addEventListener("click", question14orrect);
 
-document.getElementsByClassName("q4Wrong").addEventListener("click", question4Wrong);
+// document.getElementsByClassName("q4Wrong").addEventListener("click", question4Wrong);
 
-function question4Correct() {
-  document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
-}
-function question4Wrong() {
-  document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
-}
+// function question4Correct() {
+//   document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
+// }
+// function question4Wrong() {
+//   document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
+// }
