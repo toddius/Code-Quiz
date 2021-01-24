fae1d1e91f475a2602a9f35c20101f591d373f95
Todd Walls <t_e_walls@yahoo.com>
2021-01-23 23:38:03 -0700

    I redid the html and js. I created a container in html to hold the question with choices and I set up the questions with answers as an array holding objects. I need to figure out how to listen to an answer click and determine if it is correct or not. I need to change the time if it is wrong. I need to figure out how to end the game by time or completion and I need to figure out how to enter high score with initials and start the game over. I also need to do the ReadMe file.

--- Code Quiz/Assets/JS/script.js
+++ Code Quiz/Assets/JS/script.js
@@ -1,59 +1,83 @@
 let timeLeft = 0;
+let quiz = [
+  {
+    "question" : "Commonly used data types DO NOT include:",
+    "ans1" : "1. strings",
+    "ans2" : "2. booleans",
+    "ans3" : "3. alerts",
+    "ans4" : "4. numbers",
+    "correctAns" : "3. alerts"
+  },
+
+  {
+    "question" : "The condition in an if / else staement is enclosed within _________.",
+    "ans1" : "1. quotes",
+    "ans2" : "2. curly brackets",
+    "ans3" : "3. parentheses",
+    "ans4" : "4. square brackets",
+    "correctAns" : "3. parentheses"
+  },
+
+  {
+    "question" : "Arrays within JavaScript can be used to store _________.",
+    "ans1" : "1. strings",
+    "ans2" : "2. other arrays",
+    "ans3" : "3. booleans",
+    "ans4" : "4. all of the above",
+    "correctAns" : "4. all of the above"
+  },
+
+  {
+    "question" : "String values must be enclosed within _________ when being assigned to variables.",
+    "ans1" : "1. commas",
+    "ans2" : "2. curly brackets",
+    "ans3" : "3. quotes",
+    "ans4" : "4. parentheses",
+    "correctAns" : "3. quotes"
+  },
+
+  {
+    "question" : "A very useful tool used during development and debugging for printing content to the debugger is:",
+    "ans1" : "1. JavaScript",
+    "ans2" : "2. terminal / bash",
+    "ans3" : "3. for loops",
+    "ans4" : "4. console log",
+    "correctAns" : "4. console log"
+  }
+]
 
 document.getElementById("startButton").addEventListener("click", startGame);
 
-function startGame() {
-  document.getElementById("gameIntro").innerHTML = document.getElementById("question1").innerHTML;
-
-    timeleft = 75;
-    var downloadTimer = setInterval(function(){
-    timeleft--;
-    document.getElementById("countDownTimer").innerHTML = timeleft;
-    if(timeleft <= 0)
-        clearInterval(downloadTimer);
-    },1000);
+function startTimer () {
+  timeleft = 75;
+  var downloadTimer = setInterval(function(){
+  timeleft--;
+  document.getElementById("countDownTimer").innerHTML = timeleft;
+  if(timeleft <= 0)
+      clearInterval(downloadTimer);
+  },1000);
 }
 
-// document.getElementById("q1Correct").addEventListener("click", question1Correct);
+function startGame() {
+  startTimer();
+  document.getElementById("gameIntro").style.display = "none";
+  document.getElementById("gamePlay").style.display = "inline";
+  
+    for(let quiziz of quiz) {
+      // console.log(quiziz.question, quiziz.ans1);
+    document.getElementById("question").textContent = quiziz.question;
+    document.getElementById("answer1").textContent = quiziz.ans1;
+    document.getElementById("answer2").textContent = quiziz.ans2;
+    document.getElementById("answer3").textContent = quiziz.ans3;
+    document.getElementById("answer4").textContent = quiziz.ans4;
 
-// document.getElementsByClassName("q1Wrong").addEventListener("click", question1Wrong);
+    var choice = document.querySelector("button").addEventListener("click", () => {
+      console.log(choice);
+      // if (choice === quiz.correctAns) {
+    } );
+    
+     
 
-// function question1Correct() {
-//   document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
-// }
-// function question1Wrong() {
-//   document.getElementById("question1").innerHTML = document.getElementById("question2").innerHTML;
-// }
+      }
 
-// document.getElementById("q2Correct").addEventListener("click", question2Correct);
-
-// document.getElementsByClassName("q2Wrong").addEventListener("click", question2Wrong);
-
-// function question2Correct() {
-//   document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
-// }
-// function question2Wrong() {
-//   document.getElementById("question2").innerHTML = document.getElementById("question3").innerHTML;
-// }
-
-// document.getElementById("q3Correct").addEventListener("click", question3Correct);
-
-// document.getElementsByClassName("q3Wrong").addEventListener("click", question3Wrong);
-
-// function question3Correct() {
-//   document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
-// }
-// function question3Wrong() {
-//   document.getElementById("question3").innerHTML = document.getElementById("question4").innerHTML;
-// }
-
-// document.getElementById("q4Correct").addEventListener("click", question14orrect);
-
-// document.getElementsByClassName("q4Wrong").addEventListener("click", question4Wrong);
-
-// function question4Correct() {
-//   document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
-// }
-// function question4Wrong() {
-//   document.getElementById("question1").innerHTML = document.getElementById("question5").innerHTML;
-// }
+  }
