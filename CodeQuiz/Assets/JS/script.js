ad3c46401ebcd79bcdde767cf5314e628dc1edd8
Todd Walls <t_e_walls@yahoo.com>
2021-01-24 21:43:53 -0700

    After 8 hours today, I made no progress. I am at a loss and need to get help to finish thsi assignment.

--- CodeQuiz/Assets/JS/script.js
+++ CodeQuiz/Assets/JS/script.js
@@ -62,16 +62,15 @@ function startTimer () {
 function startGame() {
   startTimer();
   document.getElementById("gameIntro").style.display = "none";
-  document.getElementById("gamePlay").style.display = "inline";
+  document.getElementById("gamePlay").style.display = "inline-flex";
   
-    for(let quiziz of quiz) {
-      // console.log(quiziz.question, quiziz.ans1);
-    document.getElementById("question").textContent = quiziz.question;
-    document.getElementById("answer1").textContent = quiziz.ans1;
-    document.getElementById("answer2").textContent = quiziz.ans2;
-    document.getElementById("answer3").textContent = quiziz.ans3;
-    document.getElementById("answer4").textContent = quiziz.ans4;
-
+  if (choice === "" && timeleft != "0") {
+    for (i=0; i < quiz.length; i++) {
+    document.getElementById("question").textContent = quiz.question[i];
+    document.getElementById("answer1").textContent = quiz.ans1[i];
+    document.getElementById("answer2").textContent = quiz.ans2[i];
+    document.getElementById("answer3").textContent = quiz.ans3[i];
+    document.getElementById("answer4").textContent = quiz.ans4[i];
 
   //   var choice = document.getElementsByClassName("btn");
   //   function showChoice() {
@@ -109,22 +108,22 @@ for (var i = 0; i <= buttons.length; i += 1) {
     //   if (choice.textContent === quiz.correctAns) {
     //     var line1 = document.createElement("hr");
     //     document.getElementById("gamePlay").appendChild(line1);
-    //     var para1 = document.createElement("P");                       
+    //     var para1 = document.createElement("P");                       // Create a <p> node
     //     var text1 = document.createTextNode("Correct!");
-    //     para1.appendChild(text1);                                          
+    //     para1.appendChild(text1);                                          // Append the text to <p>
     //     document.getElementById("gamePlay").appendChild(para1);
     //     correctTimer();
     // } else {
     //     var line2 = document.createElement("hr");
     //     document.getElementById("gamePlay").appendChild(line2);
-    //     var para2 = document.createElement("P");                       
+    //     var para2 = document.createElement("P");                       // Create a <p> node
     //     var text2 = document.createTextNode("Wrong!");
-    //     para2.appendChild(text2);                                          
+    //     para2.appendChild(text2);                                          // Append the text to <p>
     //     document.getElementById("gamePlay").appendChild(para2);
     //     wrongTimer();
     // }
     
     //   });
     }
-  
+  }
 
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Code-Quiz
 timed quiz about Javascript facts
+
+I first created the HTML file to produce the general structure I saw in the gif provided. I created a nav bar with a link to the high scores (not active as pof this point) and the countdown time (which is working). 
+
+I also create a centerd container that could hold multiple divs that could be revealed or hidden with CSS. I was able to produce the home page to start the game and hide it with a click of the start button and produce the div that holds the quesiton with answers. I started working on the end of game div and did not start the high scores div that would hold all the high scores.
+
+Initially, I made separate divs for all the questions, but after speaking with a TA, they recommended I put all the questions and answers into an array of objects. After I did that, I tried to create a loop that would populate the div elements with the questions and answers. I got it to 'work' but it always went to the end of the array. 
+
+Another problem I am having is deterrmining which answer the user chooses so I can determine if it is right or wrong and code for what the game to do next. 
+
+I just need to solve how to create the quesitons one at a time and determine the answer chosen, then I can move forward to completing this game. 
+
+![Start page](/Code Quiz/Assets/screen shots/Screen Shot 2021-01-24 at 9.15.58 PM.png?raw=true "Start page")
+
+![First Question](/Code Quiz/Assets/screen shots/Screen Shot 2021-01-24 at 9.37.32 PM.png?raw=true "First Question")
+
+Here is the link to the game:  https://toddius.github.io/Code-Quiz/
+
