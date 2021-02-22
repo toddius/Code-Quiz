f8b033d7124d085f5f15a524adbf966b888a1929
Todd Walls <t_e_walls@yahoo.com>
2021-02-21 22:11:29 -0700

    made significant progress towards geting this ready for resubmission. All that is left to do: stop the time when the game is over, log the time at ending as the final score, save initials with final score to local stoarage, link top scores in header to top scores page called from local storage and updating readme.

--- CodeQuiz/Assets/JS/script.js
+++ CodeQuiz/Assets/JS/script.js
@@ -1,53 +1,41 @@
 let timeleft = 0;
-let choice = "";
+let currentQuestionIndex = 0;
+let questionDiv = document.getElementById("gamePlay");
+let finalScore = 0;
+
 let quiz = [
   {
     "question" : "Commonly used data types DO NOT include:",
-    "ans1" : "1. strings",
-    "ans2" : "2. booleans",
-    "ans3" : "3. alerts",
-    "ans4" : "4. numbers",
-    "correctAns" : "3. alerts"
+    "answers" : ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    "correctAnswer" : 2
   },
 
   {
-    "question" : "The condition in an if / else staement is enclosed within _________.",
-    "ans1" : "1. quotes",
-    "ans2" : "2. curly brackets",
-    "ans3" : "3. parentheses",
-    "ans4" : "4. square brackets",
-    "correctAns" : "3. parentheses"
+    "question" : "The condition in an if / else statement is enclosed within _________.",
+    "answers" : ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    "correctAnswer" : 1
   },
 
   {
     "question" : "Arrays within JavaScript can be used to store _________.",
-    "ans1" : "1. strings",
-    "ans2" : "2. other arrays",
-    "ans3" : "3. booleans",
-    "ans4" : "4. all of the above",
-    "correctAns" : "4. all of the above"
+    "answers" : ["1. strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    "correctAnswer" : 3
   },
 
   {
     "question" : "String values must be enclosed within _________ when being assigned to variables.",
-    "ans1" : "1. commas",
-    "ans2" : "2. curly brackets",
-    "ans3" : "3. quotes",
-    "ans4" : "4. parentheses",
-    "correctAns" : "3. quotes"
+    "answers" : ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    "correctAnswer" : 2
   },
 
   {
     "question" : "A very useful tool used during development and debugging for printing content to the debugger is:",
-    "ans1" : "1. JavaScript",
-    "ans2" : "2. terminal / bash",
-    "ans3" : "3. for loops",
-    "ans4" : "4. console log",
-    "correctAns" : "4. console log"
+    "answers" : ["1. JavaScript", "2. terminal / bash", "3. for loops", "4. console log"],
+    "correctAnswer" : 3
   }
-]
-
-let test = document.getElementById("startButton").addEventListener("click", startGame);
+];
+let pointer = quiz[currentQuestionIndex];
+let currentQuestion = "<h2>" + pointer.question + "</h2><ul></ul>";
 
 function startTimer () {
   timeleft = 75;
@@ -55,75 +43,67 @@ function startTimer () {
   timeleft--;
   document.getElementById("countDownTimer").textContent = timeleft;
   if(timeleft <= 0)
-      clearInterval(downloadTimer);
+      clearInterval();
+      // scoreBoard(downloadTimer);
   },1000);
 }
 
-function startGame() {
+
+function scoreBoard (data) {
+  // $("h2").remove();
+  // $("ul").remove();
+  console.log(data);
+  $("#gamePlay").remove();
+  $("#finalScore").attr("style", "display:inline-flex");
+}
+
+function nextQuestion (Index) {
+  if (Index <= 4) {
+    showQuestionIndex(Index);
+  } else {
+    // clearInterval(downloadTimer);
+    scoreBoard ();
+  } 
+}
+
+
+
+function showQuestionIndex (currentQuestionIndex) {
+  pointer = quiz[currentQuestionIndex];
+  currentQuestion = "<h2>" + pointer.question + "</h2><ul></ul>";
+  questionDiv.innerHTML = currentQuestion;
+  let answerList = questionDiv.children[1]
+  for (let i = 0; i < pointer.answers.length; i++) {
+    answerList.innerHTML += ('<li id="'+i+'">' + pointer.answers[i] + '</li>');
+  }
+
+  $( "#gamePlay ul li" ).on( "click", function() {
+    // console.log( $( this ).attr('id') );
+    currentQuestionIndex++;
+    var previousQuestionIndex = currentQuestionIndex - 1;
+    console.log(previousQuestionIndex);
+    $("h2").remove();
+    $("ul").remove();
+    if ($(this).attr('id') != quiz[previousQuestionIndex].correctAnswer){
+    // console.log(currentQuestionIndex-1);
+    nextQuestion(currentQuestionIndex);
+    timeleft = timeleft-10;
+    }
+    else {
+      nextQuestion(currentQuestionIndex);
+    }
+    
+  });
+}
+
+function startGame () {
   startTimer();
+  
   document.getElementById("gameIntro").style.display = "none";
   document.getElementById("gamePlay").style.display = "inline-flex";
   
-  if (choice === "" && timeleft != "0") {
-    for (i=0; i < quiz.length; i++) {
-    document.getElementById("question").textContent = quiz.question[i];
-    document.getElementById("answer1").textContent = quiz.ans1[i];
-    document.getElementById("answer2").textContent = quiz.ans2[i];
-    document.getElementById("answer3").textContent = quiz.ans3[i];
-    document.getElementById("answer4").textContent = quiz.ans4[i];
-
-  //   var choice = document.getElementsByClassName("btn");
-  //   function showChoice() {
-  //     var place = document.getElementById('textfield');
-  //     var choiceBox = document.createElement('textarea');
-  //     place.appendChild(choiceBox);
-  //   }
-  //   for (var i = 0 ; i < choice.length; i++) {
-  //     choice[i].addEventListener('click' , showChoice , false ) ; 
-  //  }
-
-// let practice = document.querySelector("button").addEventListener("click", nextQuestion);
-
-// $('#group2').on('click', 'button', function(event) {
-//   var name = this.textContent
-//   var r = $('<input type="button" value="' + name + '">');
-//   $("div#group1").append(r);
-// });
-
-// let selection = document.querySelector("button").addEventListener("click" , answerChoice);
-
-// function answerChoice () {
-//   choice = selection.id;
-//   let choiceText = choice.textContent;
-//   console.log(choiceText);
-var buttons = document.getElementsByTagName("button");
-/* var buttonsCount = buttons.length; */
-for (var i = 0; i <= buttons.length; i += 1) {
-    buttons[i].onclick = function(e) {
-        alert(this.id);
-    };
-}
+  showQuestionIndex (currentQuestionIndex);
 
 }
-    //   if (choice.textContent === quiz.correctAns) {
-    //     var line1 = document.createElement("hr");
-    //     document.getElementById("gamePlay").appendChild(line1);
-    //     var para1 = document.createElement("P");                       // Create a <p> node
-    //     var text1 = document.createTextNode("Correct!");
-    //     para1.appendChild(text1);                                          // Append the text to <p>
-    //     document.getElementById("gamePlay").appendChild(para1);
-    //     correctTimer();
-    // } else {
-    //     var line2 = document.createElement("hr");
-    //     document.getElementById("gamePlay").appendChild(line2);
-    //     var para2 = document.createElement("P");                       // Create a <p> node
-    //     var text2 = document.createTextNode("Wrong!");
-    //     para2.appendChild(text2);                                          // Append the text to <p>
-    //     document.getElementById("gamePlay").appendChild(para2);
-    //     wrongTimer();
-    // }
-    
-    //   });
-    }
-  }
 
+let test = document.getElementById("startButton").addEventListener("click", startGame);
