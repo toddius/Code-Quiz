9f104a2c983cb23430d0e9141a41e0f7085a70d7
Todd Walls <t_e_walls@yahoo.com>
2021-03-03 11:13:05 -0700

    mor emodiifcations made. need to figure put how to get local storage to add to high score list and how to reset the game

--- CodeQuiz/Assets/JS/script.js
+++ CodeQuiz/Assets/JS/script.js
@@ -2,7 +2,8 @@ let timeleft = 0;
 let currentQuestionIndex = 0;
 let questionDiv = document.getElementById("gamePlay");
 let finalScore = 0;
-
+let quizComplete = false;
+let userData = [];
 let quiz = [
   {
     "question" : "Commonly used data types DO NOT include:",
@@ -42,31 +43,39 @@ function startTimer () {
   var downloadTimer = setInterval(function(){
   timeleft--;
   document.getElementById("countDownTimer").textContent = timeleft;
-  if(timeleft <= 0)
-      clearInterval();
-      // scoreBoard(downloadTimer);
+  if(timeleft <= 0 || quizComplete === true)
+      clearInterval(downloadTimer);
   },1000);
 }
 
 
 function scoreBoard (data) {
-  // $("h2").remove();
-  // $("ul").remove();
-  console.log(data);
+  // console.log(data);
   $("#gamePlay").remove();
   $("#finalScore").attr("style", "display:inline-flex");
+  $("#finalCountDownTimer").text(data.toString());
 }
 
 function nextQuestion (Index) {
   if (Index <= 4) {
     showQuestionIndex(Index);
   } else {
-    // clearInterval(downloadTimer);
-    scoreBoard ();
+    quizComplete = true;
+    scoreBoard (timeleft-1);
   } 
 }
 
+function getDisplayScores(formValue, score) {
+  var userInitials = formValue.val();
+  var userScore = score;
+  localStorage.setItem(userInitials, userScore);
+  showHighScores();
+}
 
+$("form").on("submit", function(e) {
+  e.preventDefault();
+  getDisplayScores($("#inputInitials"), timeleft);
+})
 
 function showQuestionIndex (currentQuestionIndex) {
   pointer = quiz[currentQuestionIndex];
@@ -92,18 +101,37 @@ function showQuestionIndex (currentQuestionIndex) {
     else {
       nextQuestion(currentQuestionIndex);
     }
-    
   });
 }
 
 function startGame () {
   startTimer();
-  
   document.getElementById("gameIntro").style.display = "none";
-  document.getElementById("gamePlay").style.display = "inline-flex";
-  
+  document.getElementById("gamePlay").style.display = "block";
   showQuestionIndex (currentQuestionIndex);
+}
 
+function startOver() {
+  document.getElementById("highScores").style.display = "none";
+  document.getElementById("gameIntro").style.display = "inline-flex";
+  document.getElementById("navigationBar").style.display = "inline-flex";
+  // startGame();
+}
+
+function clearHighScores () {
+
+}
+
+function showHighScores (formValue) {
+  // $("#finalScore").remove();
+  document.getElementById("finalScore").style.display = "none";
+  document.getElementById("navigationBar").style.display = "none";
+  document.getElementById("highScores").style.display = "inline-flex";
+  var scores = localStorage.getItem(userInitials, userScore);
+  console.log(scores);
+  // document.getElementById("scoreList").appendChild(scores);
+  // document.getElementById("goBack").addEventListener("click", startOver);
+  // document.getElementById("clearScores").addEventListener("click", clearHighScores);
 }
 
 let test = document.getElementById("startButton").addEventListener("click", startGame);
